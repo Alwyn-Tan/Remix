d94df241d6a42709e0c76236b8a795eba1c493d2
Alwyn-Tan <2660412180@qq.com>
2024-09-20 21:18:42 +0800

    fix: Finish Storage.sol

--- contracts/1_Storage.sol
+++ contracts/1_Storage.sol
@@ -9,14 +9,26 @@ pragma solidity >=0.8.2 <0.9.0;
  */
 contract Storage {
 
+    uint256 num;
+
     struct Person{
         uint256 number;
         string name;
     }
 
     Person[] public people;
+    mapping(string=>uint256) public map;
+
+    function store(uint256 _num) public {
+        num = _num;
+    }
+
+    function retrieve() public view returns (uint256) {
+        return num;
+    }
 
     function addPerson(string memory _name, uint _number) public{
         people.push( Person(_number, _name));
+        map[_name] = _number;
     }
 }
